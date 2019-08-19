cc5974154f1fa857a35e2b25ee11d67843f77146
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-08-19 11:44:52 +0700

    [Edited]: แก้ไขเป็น package test

--- ConvertToFizzBuzz_test.go
+++ ConvertToFizzBuzz_test.go
@@ -1,13 +1,15 @@
-package FizzBuzz
+package fizzbuzz_test
 
 import (
 	"testing"
+
+	fizzbuzz "github.com/Kanokpron10/FizzBuzz"
 )
 
 func Test_ConvertToFizzBuzz_Input_1_Should_Be_1(t *testing.T) {
 	expectedResult := "1"
 
-	actualResult := ConvertToFizzBuzz(1)
+	actualResult := fizzbuzz.ConvertToFizzBuzz(1)
 
 	if expectedResult != actualResult {
 		t.Errorf("Expect %s but got %s", expectedResult, actualResult)
@@ -17,7 +19,7 @@ func Test_ConvertToFizzBuzz_Input_1_Should_Be_1(t *testing.T) {
 func Test_ConvertToFizzBuzz_Input_3_Should_Be_Fizz(t *testing.T) {
 	expectedResult := "Fizz"
 
-	actualResult := ConvertToFizzBuzz(3)
+	actualResult := fizzbuzz.ConvertToFizzBuzz(3)
 
 	if expectedResult != actualResult {
 		t.Errorf("Expect %s but got %s", expectedResult, actualResult)
@@ -27,7 +29,7 @@ func Test_ConvertToFizzBuzz_Input_3_Should_Be_Fizz(t *testing.T) {
 func Test_ConvertToFizzBuzz_Input_5_Should_Be_Buzz(t *testing.T) {
 	expectedResult := "Buzz"
 
-	actualResult := ConvertToFizzBuzz(5)
+	actualResult := fizzbuzz.ConvertToFizzBuzz(5)
 
 	if expectedResult != actualResult {
 		t.Errorf("Expect %s but got %s", expectedResult, actualResult)
@@ -37,7 +39,7 @@ func Test_ConvertToFizzBuzz_Input_5_Should_Be_Buzz(t *testing.T) {
 func Test_ConvertToFizzBuzz_Input_6_Should_Be_Fizz(t *testing.T) {
 	expectedResult := "Fizz"
 
-	actualResult := ConvertToFizzBuzz(6)
+	actualResult := fizzbuzz.ConvertToFizzBuzz(6)
 
 	if expectedResult != actualResult {
 		t.Errorf("Expect %s but got %s", expectedResult, actualResult)
@@ -47,7 +49,7 @@ func Test_ConvertToFizzBuzz_Input_6_Should_Be_Fizz(t *testing.T) {
 func Test_ConvertToFizzBuzz_Input_15_Should_Be_FizzBuzz(t *testing.T) {
 	expectedResult := "FizzBuzz"
 
-	actualResult := ConvertToFizzBuzz(15)
+	actualResult := fizzbuzz.ConvertToFizzBuzz(15)
 
 	if expectedResult != actualResult {
 		t.Errorf("Expect %s but got %s", expectedResult, actualResult)
